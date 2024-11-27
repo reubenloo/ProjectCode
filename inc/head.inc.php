e270e1e9184f5adbdf071b4b751e5e273ec444f4
Reuben Loo <54463051+reubenloo@users.noreply.github.com>
2024-11-27 20:18:23 +0800

    Added JS bundle
    Make mobile menu work

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -4,6 +4,8 @@
     <title>Homepage</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Any custom CSS -->
     <link rel="stylesheet" href="css/main.css">
 </head>
